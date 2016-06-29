f0f36bd734bb6596905f7d246f90d692115dc81c
taoshuang <Cool_tao@qq.com>
2016-06-29 08:43:22 +0800

    DevcesUtil添加像素和Dp转换

--- app/src/main/java/example/mysmallexample/ui/utils/DeviceUtil.java
+++ app/src/main/java/example/mysmallexample/ui/utils/DeviceUtil.java
@@ -1,13 +1,14 @@
 package example.mysmallexample.ui.utils;
 
+import android.content.Context;
 import android.os.Build;
 
-/**
- * 判断是不是虚拟设备
- * Utility methods related to physical devies and emulators.
- */
-public class DeviceUtil {
 
+public class DeviceUtil {
+    /**
+     * 判断是不是虚拟设备
+     * Utility methods related to physical devies and emulators.
+     */
     public static boolean isEmulator() {
         return Build.FINGERPRINT.startsWith("generic")
                 || Build.FINGERPRINT.startsWith("unknown")
@@ -17,5 +18,29 @@ public class DeviceUtil {
                 || Build.MANUFACTURER.contains("Genymotion")
                 || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
                 || "google_sdk".equals(Build.PRODUCT);
+    }
+
+    /**
+     * dp转换为px
+     *
+     * @param context
+     * @param dp
+     * @return
+     */
+    public static int Dp2Px(Context context, float dp) {
+        float density = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * density + 0.5f);
+    }
+
+    /**
+     * px转换为dp
+     *
+     * @param context
+     * @param px
+     * @return
+     */
+    public static int Px2Dp(Context context, float px) {
+        final float density = context.getResources().getDisplayMetrics().density;
+        return (int) (px / density + 0.5f);
     }
 }
